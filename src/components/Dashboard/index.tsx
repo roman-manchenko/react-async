95f395df7e981977fb74cff6e69b5f5f7297dfe7
Roman Manchenko <manchenko.roman@gmail.com>
2016-11-09 20:21:50 -0500

    attempt to update state via redux

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,22 +1,29 @@
 import * as React from "react";
 import { Router, Route, browserHistory, IndexLink} from "react-router";
 import { ListItemComponent } from "../../listItemComponent"
+import { dispatch } from "../../store"
+import { createListItemAction, ListItem } from "../../redux-actions"
 
 interface DashboardConfig {
   config: string;
   userSettings: string;
 }
 
-class DashboardApp extends React.Component<DashboardConfig, {}> {
-  render() {
-      let listItem = {listItem: {'done': false, 'description': 'Fuck it all !!!'}}
-      let index = 0
+class DashboardApp extends React.Component<any, any> {
+  renderItems() {
 
+  }
+
+  render() {
       return (
         <div className="main clearfix">
           <div id="dash-tab-title" className="dash-tab-title">
-            <ListItemComponent {...listItem} index={index} />
+            {this.state && this.state.listItems && this.state.listItems.map(function (listItem: ListItem, index: number) {
+              <ListItemComponent {...listItem} index={index} />
+            })
+            }
           </div>
+          <button onClick={() => (dispatch(createListItemAction({done: false, description: 'fuck it twice!'})))}>Create</button>
           <div id="subtabs-menu" className="sub-tabs">
             <ul>
               <li><IndexLink to="/" activeClassName="active">Home</IndexLink></li>
